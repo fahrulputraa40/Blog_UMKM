861f48496ca26669ad02a2bbfac29c3d3cd383f8
fahrulputra40 <fahrulputra40@gmail.com>
2021-10-01 14:19:11 +0700

    footer fix

--- wp-content/themes/thisite/footer.php
+++ wp-content/themes/thisite/footer.php
@@ -1,8 +1,46 @@
 <footer>
     <div class="footer-icon">
-        <img src=<?php echo get_theme_file_uri('/assets/bumn.png');?> alt="">
+        <img src=<?php echo get_theme_file_uri('/assets/bumn.png'); ?> alt="">
+    </div>
+    <div class="footer-contents mt-4">
+        <div class="office mr-sm-4">
+            <h5>Office</h5>
+            <p>Kompleks PT. Indmira, Jl. Kaliurang km 16,3, Kledokan, Ngemplak, Sleman, Daerah Istimewa Yogyakarta, Indonesia 55585</p>
+        </div>
+        <div class="contact">
+            <h5>Support</h5>
+            <div class="phone">
+                <i class="fa fa-phone-square mr-2"></i>
+                08123456789 (Adit)
+            </div>
+        </div>
+        <div class="me">
+            <h5>About</h5>
+            <ul>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Blog</a></li>
+            </ul>
+        </div>
+        <div class="quot">
+            <h5>Random Quote</h5>
+            <i>
+            “Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.”
+            </i>
+            <p class="mt-2" style="margin: 0;">-Steve Jobs</p>
+        </div>
+    </div>
+    <div class="policy">
+        <span class="term">
+            © 2021 Fahrul Putra. All rights reserved
+        </span>
+        <span class="social">
+            <a href=""><i class="fab fa-facebook-f"></i></a>
+            <a href=""><i class="fab fa-twitter"></i></a>
+            <a href=""><i class="fab fa-youtube"></i></a>
+        </span>
     </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
